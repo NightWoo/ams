79547f552c2fe7a634489d76c38479b47b0df1e4
WuJun <nightwoo@163.com>
2013-03-15 10:45:42 +0800

    head adjust

--- protected/views/common/head.php
+++ protected/views/common/head.php
@@ -2,12 +2,12 @@
 <div class="navbar navbar-fixed-top" id="bmsHead">
 	<div class="navbar-inner">
 		<div class="container">
-			<a class="brand" href="#">AMS</a>
+			<a class="brand" href="/bms/site">AMS</a>
 			<div class="nav-collapse">
 				<ul class="nav">
-					<li ><a href="/bms/site">首页</a></li>
+					<!-- <li ><a href="/bms/site">首页</a></li> -->
 					<li class="dropdown" id="headManagementSystemLi">
-						<a href="/bms/ManagementSystem/home?chapter=0" class="dropdown-toggle" >管理体系</a>
+						<a href="/bms/ManagementSystem/home?chapter=0" class="dropdown-toggle">体系</a>
 						<!-- <ul class="dropdown-menu">
 							<li><a href="">体系概况</a></li>
 							<li><a href="zz_nodeselect.php">管理考核与评价</a></li>
@@ -15,7 +15,7 @@
 						</ul> -->
 					</li>
 					<li class="dropdown" id="headAssemblyLi">
-						<a href="/bms/execution" class="dropdown-toggle" >生产执行</a>
+						<a href="/bms/execution" class="dropdown-toggle" >生产</a>
 						<!-- <ul class="dropdown-menu">
 							<li><a href="/bms/execution/home">总装</a></li>
 							<li><a href="#">涂装</a></li>
@@ -24,16 +24,19 @@
 						</ul> -->
 					</li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">现场管理<b class="caret"></b></a>
-						<ul class="dropdown-menu">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">现场<!-- <b class="caret"></b> --></a>
+						<!-- <ul class="dropdown-menu">
 							<li><a href="#">现场管理1</a></li>
 							<li><a href="#">现场管理2</a></li>
 							<li><a href="#">现场管理3</a></li>
-						</ul>
+						</ul> -->
 					</li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">质量管理<b class="caret"></b></a>
-						<ul class="dropdown-menu">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">计划<!-- <b class="caret"></b> --></a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">质量<!-- <b class="caret"></b> --></a>
+						<!-- <ul class="dropdown-menu">
 							<li><a href="#">质量工程</a></li>
 							<li><a href="#">IQS售后质量</a></li>
 							<li><a href="#">DRR内部质量</a></li>
@@ -41,31 +44,31 @@
 							<li><a href="#">CPA产品审核</a></li>
 							<li><a href="#">LPA过程审核</a></li>
 							<li><a href="#">Lesson Learned</a></li>
-						</ul>
+						</ul> -->
 					</li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">成本管理<b class="caret"></b></a>
-						<ul class="dropdown-menu">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">成本<!-- <b class="caret"></b> --></a>
+						<!-- <ul class="dropdown-menu">
 							<li><a href="#">成本管理1</a></li>
 							<li><a href="#">成本管理2</a></li>
-						</ul>
+						</ul> -->
 					</li>
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">资源管理<b class="caret"></b></a>
-						<ul class="dropdown-menu">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">人事<!-- <b class="caret"></b> --></a>
+						<!-- <ul class="dropdown-menu">
 							<li><a href="#">人力资源管理</a></li>
 							<li><a href="#">文件记录管理</a></li>
 							<li><a href="#">工具量具管理</a></li>
 							<li><a href="#">培训管理</a></li>
-						</ul>
+						</ul> -->
 					</li>
-					<li class="dropdown" id="headGeneralInformationLi">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">综合信息<b class="caret"></b></a>
-						<ul class="dropdown-menu">
+					<li id="headGeneralInformationLi">
+						<a href="/bms/generalInformation/faultMaintain" class="dropdown-toggle">数据库<!-- <b class="caret"></b> --></a>
+						<!-- <ul class="dropdown-menu">
 							<li><a href="#">情报中心</a></li>
 							<li><a href="/bms/generalInformation/generalIndex">基础数据库</a></li>
 							<li><a href="#">维护与帮助</a></li>
-						</ul>
+						</ul> -->
 					</li>
 					
 				</ul>
