cbbeb97436fe695745433dc7fcdbc0aa88ed4742
WuJun <nightwoo@163.com>
2013-03-31 19:28:48 +0800

    fix

--- js/common.js
+++ js/common.js
@@ -87,23 +87,97 @@ $(function () {
 		},
 
 		todayEndTime : function () {
+			// var now = new Date();
+	  //       var year = now.getFullYear();       //年
+	  //       var month = now.getMonth() + 1;     //月
+	  //       var day = now.getDate();            //日
+	       
+	  //       var clock = year + '-';
+
+	  //       if(month < 10) clock += '0';
+	  //       clock += month + '-';
+
+	  //       if(day < 10) clock += '0';
+	  //       clock += (day + 1) + ' ';
 			var now = new Date();
-		        var year = now.getFullYear();       //年
-		        var month = now.getMonth() + 1;     //月
-		        var day = now.getDate();            //日
-		       
-		        var clock = year + '-';
+			var hh = now.getHours();
+			if(hh>=8 && hh<24) {
+				endClock = self.byd.DateUtil.tomorrowDate();
+			} else {
+				endClock = self.byd.DateUtil.currentDate();
+			}
+			console.log(self);
+	        endClock += " 07:59";
 
-		        if(month < 10) clock += '0';
-		        clock += month + '-';
+	        return(endClock); 
+		},
 
-		        if(day < 10) clock += '0';
-		        clock += (day + 1) + ' ';
+		tomorrowDate: function () {
+			//获取系统时间 
+			var now = new Date();
+			var nowYear = now.getFullYear();
+			var nowMonth = now.getMonth();
+			var nowDate = now.getDate();
+			//处理
+			var uom = new Date(nowYear,nowMonth, nowDate);
+			uom.setDate(uom.getDate() + 1);//取得系统时间的前一天,重点在这里,负数是前几天,正数是后几天
+			var LINT_MM = uom.getMonth();
+			LINT_MM++;
+			var LSTR_MM = LINT_MM >= 10?LINT_MM:("0"+LINT_MM)
+			var LINT_DD = uom.getDate();
+			var LSTR_DD = LINT_DD >= 10?LINT_DD:("0"+LINT_DD)
+			//得到最终结果
+			uom = uom.getFullYear() + "-" + LSTR_MM + "-" + LSTR_DD; 
+			return(uom); 
+		},
 
-		        clock += "07:59";
+		yesterday: function () {			
+			//获取系统时间 
+			var now = new Date();
+			var nowYear = now.getFullYear();
+			var nowMonth = now.getMonth();
+			var nowDate = now.getDate();
+			//处理
+			var uom = new Date(nowYear,nowMonth, nowDate);
+			uom.setDate(uom.getDate()-1);//取得系统时间的前一天,重点在这里,负数是前几天,正数是后几天
+			var LINT_MM = uom.getMonth();
+			LINT_MM++;
+			var LSTR_MM = LINT_MM > 10?LINT_MM:("0"+LINT_MM)
+			var LINT_DD = uom.getDate();
+			var LSTR_DD = LINT_DD > 10?LINT_DD:("0"+LINT_DD)
+			//得到最终结果
+			uom = uom.getFullYear() + "-" + LSTR_MM + "-" + LSTR_DD; 
+			return(uom);
+		},
 
-		        return(clock); 
-		}
+		workDate: function () {
+			var now = new Date();
+			var hh = now.getHours();
+			var workDate;
+
+			if(hh>=8 && hh<24) {
+				workDate = self.byd.DateUtil.currentDate();
+			} else {
+				workDate = self.byd.DateUtil.yesterday();
+			}
+
+			return(workDate);
+		},
+
+		nextWorkDate: function () {
+			var now = new Date();
+			var hh = now.getHours();
+			var nextWorkDate
+
+			if(hh>=8 && hh<24) {
+				nextWorkDate = self.byd.DateUtil.tomorrowDate();
+			} else {
+				nextWorkDate = self.byd.DateUtil.currentDate();
+			}
+
+			return nextWorkDate;
+		},	
+
 	};/* END DataUtil CLASS DEFINITION */
 
 
